8eb2f6ce44e567b6490f47ec4b90f17965f0b751
SABI9666 <sabincn676@gmail.com>
2025-12-09 18:40:59 +0530

    Update gmail.js

--- routes/gmail.js
+++ routes/gmail.js
@@ -1,13 +1,18 @@
-
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 const gmailController = require('../controllers/gmailController');
 
-// Define the routes that the frontend is trying to call
+// Get Gmail OAuth URL
 router.get('/auth-url', authMiddleware, gmailController.getAuthUrl);
+
+// Gmail OAuth callback
 router.get('/callback', authMiddleware, gmailController.oauthCallback);
+
+// Disconnect Gmail
 router.post('/disconnect', authMiddleware, gmailController.disconnect);
+
+// Get Gmail connection status
 router.get('/status', authMiddleware, gmailController.getStatus);
 
 module.exports = router;
